ec8503b04bb53e614b713e4470833e301d72c2b4
Ernesto Roldan Garcia <jeroldan@gmail.com>
2016-06-03 10:03:56 +0200

    cover page router

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { Router, Route, IndexRoute } from 'react-router';
 import { Tatami, Drawer, Overlay, Toolbar, Container } from './tatami/tatami';
 import './app.css';
 import './palette-indigo.css';
 
-const App = () => (
+const Cover = () => (
+  <div className="dark-primary-color">
+    <h1>React-Tatami</h1>
+  </div>
+);
+
+const Layout = (props) => (
   <Tatami>
     <Drawer>drawer</Drawer>
-    <Container><div>container</div></Container>
+    <Container>{props.children}</Container>
     <Toolbar>toolbar</Toolbar>
     <Overlay>xxx</Overlay>
   </Tatami>
+);
+
+Layout.propTypes = {
+  children: React.PropTypes.node,
+};
+
+const App = () => (
+  <Router>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Cover} />
+    </Route>
+  </Router>
 );
 
 export default App;
