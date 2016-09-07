d40c3b77a6546de1ad2711fca09d6a67c3214681
Ernesto Roldan Garcia <jeroldan@gmail.com>
2016-09-07 11:28:48 +0200

    updated web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,21 @@
-module.exports = {
-  entry: './src/js/main.js',
-  output: {
-    path: './dist',
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  devServer: {
-    inline: true,
-    contentBase: './dist',
-    https: true,
-    host: '192.168.1.136',
-    port: 5555,
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'stage-1', 'react'],
-        },
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader',
-      },
-    ],
-  },
+const { resolve } = require('path');
+
+module.exports = env => {
+  return {
+    context: resolve(__dirname, 'src'),
+    entry: './js/main.js',
+    output: {
+      filename: 'bundle.js',
+      path: resolve(__dirname, 'dist'),
+      pathinfo: !env.prod,
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+        { test: /\.jsx?$/, loader: 'babel', exclude: /(node_modules|bower_components)/ },
+        { test: /\.css$/, loader: 'style!css' },
+        { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
+      ],
+    },
+  }
 };
